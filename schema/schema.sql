08889dad5bbdca6ed51e7a2f69e74272ff4161f9
Zach <zmoore371@gmail.com>
2022-02-04 17:30:56 -0500

    changed . to _

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,4 +1,10 @@
 DROP DATABASE IF EXISTS hourtracker;
 CREATE DATABASE hourtracker; 
 
-USE hourtracker.db;
+USE hourtracker_db;
+
+CREATE TABLE main(
+    id INT NOT NULL AUTO_INCREMENT,
+    RO INT NOT NULL,
+
+)
